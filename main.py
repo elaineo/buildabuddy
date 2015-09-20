9db0a781a3d024a3c5a3c58bd871f58317e135c8
Warren Mar <warren.mar@gmail.com>
2015-09-19 18:36:19 -0700

    update tool tip text

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ app = Flask(__name__)
 def hello():
     return render_template('starter.html')
 
+
+@app.route("/chat")
+def chat():
+    return render_template('chat.html')
+
 if __name__ == "__main__":
     app.run()
