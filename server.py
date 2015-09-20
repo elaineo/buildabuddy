a28128fcb3ac25628b111a8bcb8710279c2e9ebf
Elaine Ou <elaine.ou@gmail.com>
2015-09-20 01:59:25 -0700

    flask

--- server.py
+++ server.py
@@ -1,12 +1,29 @@
 import os
-import http.server
-import socketserver
+from flask import Flask
+from flask import render_template
+from flask import jsonify
 
 PORT = int(os.getenv('VCAP_APP_PORT', '8000'))
 
-Handler = http.server.SimpleHTTPRequestHandler
+#Handler = http.server.SimpleHTTPRequestHandler
 
-httpd = socketserver.TCPServer(("", PORT), Handler)
+#httpd = socketserver.TCPServer(("", PORT), Handler)
 
-print("serving at port", PORT)
-httpd.serve_forever()
+#print("serving at port", PORT)
+#httpd.serve_forever()
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('personality.html')
+
+
+@app.route("/chat")
+def chat():
+    return render_template('chat.html')
+
+
+if __name__ == '__main__':
+    app.run()
