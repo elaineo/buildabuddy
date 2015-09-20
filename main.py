683c6f36b26e78581a34037ec6e4eda546fab42f
Warren Mar <warren.mar@gmail.com>
2015-09-20 03:06:58 -0700

    add meet screen

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def chat():
     return render_template('chat.html')
 
 
-@app.route("/chat")
-def chat():
-    return render_template('chat.html')
+@app.route("/meet")
+def meet():
+    return render_template('meet.html')
 
 
 if __name__ == "__main__":
