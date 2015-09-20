b15b1851233e326873d236f1046b5a65ff44454c
Elaine Ou <elaine.ou@gmail.com>
2015-09-20 02:50:42 -0700

    similarity query

--- similarity.py
+++ similarity.py
@@ -5,9 +5,11 @@ import math
 # determine euclidean distance
 def similarity(origin, target):
   distance = 0.0
-  for p in origin:
-    distance += math.pow(trait.percentage - target_big5[i].percentage, 2)
-  return 1-(math.sqrt(distance/len(origin_big5)))
+  target_traits = flat_traits(target)
+  origin_traits = flat_traits(origin)
+  for o, t in zip(origin_traits, target_traits):
+    distance += math.pow(o.get("percentage") - p.get("percentage"), 2)
+  return 1-(math.sqrt(distance/len(origin_traits)))
 
 def flat_traits(person):
   traits = person.get("tree").get("children")[0].get("children")[0]
